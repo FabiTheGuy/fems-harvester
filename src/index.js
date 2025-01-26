9ce025a9fa8cb087958588db47d43c60fc6c41e8
FabiTheGuy <fabian.doeller@mailfence.com>
2025-01-26 17:52:41 +0100

    feat: add envs for docker support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 // @ts-check
+import { FemsApiClient } from './fems-api-client.js';
 
-function main() {
-    console.log('Hello, World!');
+
+// Environment variables
+const FEMS_HOST = process.env.FEMS_HOST || 'localhost';
+const FEMS_PORT = process.env.FEMS_PORT || '80';
+const FEMS_PROTOCOL = process.env.FEMS_PROTOCOL || 'http';
+const FEMS_USER = process.env.FEMS_USER || 'x';
+const FEMS_PASS = process.env.FEMS_PASS || 'user';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '5432';
+const DB_USER = process.env.DB_USER || 'postgres';
+const DB_PASS = process.env.DB_PASS || 'postgres';
+const LOG_INTERVAL = process.env.LOG_INTERVAL || '30';
+const LOG_MODULES = process.env.LOG_MODULES || 'grid_power,production_power,battery_power';
+
+// Static variables
+const femsApiClient = new FemsApiClient(FEMS_HOST, FEMS_PORT, FEMS_PROTOCOL, FEMS_USER, FEMS_PASS);
+const apiEndpointFunctions = new Map([
+    ['system_state', femsApiClient.getSystemState],
+    ['battery_charging_state', femsApiClient.getBatteryChargingState],
+    ['battery_power', femsApiClient.getBatteryPower],
+    ['battery_reactive_power', femsApiClient.getBatteryReactivePower],
+    ['grid_power', femsApiClient.getGridPower],
+    ['grid_min_power', femsApiClient.getGridMinPower],
+    ['grid_max_power', femsApiClient.getGridMaxPower],
+    ['production_power', femsApiClient.getProductionPower],
+    ['production_max_power', femsApiClient.getProductionMaxPower],
+    ['production_ac_power', femsApiClient.getProductionACPower],
+    ['production_dc_power', femsApiClient.getProductionDCPower],
+    ['consumption_power', femsApiClient.getConsumptionPower],
+    ['consumption_max_power', femsApiClient.getCosumptionMaxPower],
+]);
+
+
+async function main() {
+    const userEndpointFunctions = LOG_MODULES.split(',').map(module => apiEndpointFunctions.get(module));
+
+    console.log(userEndpointFunctions)
 }
 
 main();
