1026620b694b2309e597a9fdc662a8ece3113faf
FabiTheGuy <fabian.doeller@mailfence.com>
2025-02-06 13:13:24 +0100

    fix: changed typo + add bind to functions

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import { FemsApiClient } from './fems-api-client.js';
 
 
 // Environment variables
-const FEMS_HOST = process.env.FEMS_HOST || 'localhost';
-const FEMS_PORT = process.env.FEMS_PORT || '80';
+const FEMS_HOST = process.env.FEMS_HOST || '192.168.180.2';
+const FEMS_PORT = Number(process.env.FEMS_PORT || '80');
 const FEMS_PROTOCOL = process.env.FEMS_PROTOCOL || 'http';
 const FEMS_USER = process.env.FEMS_USER || 'x';
 const FEMS_PASS = process.env.FEMS_PASS || 'user';
 const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || '5432';
+const DB_PORT = Number(process.env.DB_PORT || '5432');
 const DB_USER = process.env.DB_USER || 'postgres';
 const DB_PASS = process.env.DB_PASS || 'postgres';
-const LOG_INTERVAL = process.env.LOG_INTERVAL || '30';
+const LOG_INTERVAL = Number(process.env.LOG_INTERVAL || '30');
 const LOG_MODULES = process.env.LOG_MODULES || 'grid_power,production_power,battery_power';
 
 // Static variables
-const femsApiClient = new FemsApiClient(FEMS_HOST, FEMS_PORT, FEMS_PROTOCOL, FEMS_USER, FEMS_PASS);
+const femsApiClient = new FemsApiClient(FEMS_HOST, FEMS_PROTOCOL, FEMS_PORT, FEMS_USER, FEMS_PASS);
 const apiEndpointFunctions = new Map([
     ['system_state', femsApiClient.getSystemState],
     ['battery_charging_state', femsApiClient.getBatteryChargingState],
@@ -35,9 +35,12 @@ const apiEndpointFunctions = new Map([
 
 
 async function main() {
-    const userEndpointFunctions = LOG_MODULES.split(',').map(module => apiEndpointFunctions.get(module));
-
-    console.log(userEndpointFunctions)
+    // Generates a set of functions that will be used according to the LOG_MODULES 
+    // environment variable to harvest data from the FEMS API.
+    const userEndpointFunctions = LOG_MODULES.split(',').map(
+        module => apiEndpointFunctions.get(module)?.bind(femsApiClient)
+    );
 }
+
 
 main();
