8ee7e2b1567ab80821ffc12f6ca81f2684ad1178
FabiTheGuy <fabian.doeller@mailfence.com>
2025-03-14 09:34:58 +0100

    chore: Initial Commit

---
+++ src/main/java/com/fabitheguy/FemsHarvester.java
@@ -0,0 +1,13 @@
+package com.fabitheguy;
+
+/**
+ * Hello world!
+ *
+ */
+public class FemsHarvester
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
